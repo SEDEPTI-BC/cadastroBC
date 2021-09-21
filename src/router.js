d8af39955c0eb2b798268e60c5f20282a578b6e9
carlosag16 <carloswiiu@gmail.com>
2021-09-21 15:02:56 -0300

    without #

--- src/router.js
+++ src/router.js
@@ -1,56 +1,57 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Index from "./views/Index.vue";
-import Landing from "./views/Landing.vue";
-import Login from "./views/Login.vue";
-import MainNavbar from "./layout/MainNavbar.vue";
-import MainFooter from "./layout/MainFooter.vue";
-import email from "./views/email.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Index from './views/Index.vue';
+import Landing from './views/Landing.vue';
+import Login from './views/Login.vue';
+import MainNavbar from './layout/MainNavbar.vue';
+import MainFooter from './layout/MainFooter.vue';
+import email from './views/email.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: "/Index",
-      name: "Index",
+      path: '/Index',
+      name: 'Index',
       components: { default: Index, header: MainNavbar, footer: MainFooter },
       props: {
         header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
+        footer: { backgroundColor: 'black' },
+      },
     },
     {
-      path: "/",
-      name: "landing",
+      path: '/',
+      name: 'landing',
       components: { default: Landing, header: MainNavbar, footer: MainFooter },
       props: {
         header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
+        footer: { backgroundColor: 'black' },
+      },
     },
     {
-      path: "/login",
-      name: "login",
+      path: '/login',
+      name: 'login',
       components: { default: Login, header: MainNavbar, footer: MainFooter },
       props: {
-        header: { colorOnScroll: 400 }
-      }
+        header: { colorOnScroll: 400 },
+      },
     },
     {
-      path: "/email",
-      name: "email",
-      components: { default: email, header: "", footer: MainFooter },
+      path: '/email',
+      name: 'email',
+      components: { default: email, header: '', footer: MainFooter },
       props: {
-        header: { colorOnScroll: 400 }
-      }
-    }
+        header: { colorOnScroll: 400 },
+      },
+    },
   ],
-  scrollBehavior: to => {
+  scrollBehavior: (to) => {
     if (to.hash) {
       return { selector: to.hash };
     } else {
       return { x: 0, y: 0 };
     }
-  }
+  },
 });
