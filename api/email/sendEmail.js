981f5d063d6b275c8dc34c5ceb26825ace875db0
Samantha Silva <samanthaathayde@hotmail.com>
2021-11-11 16:38:48 -0300

    Change form validation

--- api/email/sendEmail.js
+++ api/email/sendEmail.js
@@ -1,114 +1,113 @@
-require("dotenv").config();
-const fs = require("fs");
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
-const handlebars = require("handlebars");
-const rootDir = require("../util/path");
-const path = require("path");
+require('dotenv').config()
+const fs = require('fs')
+const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const OAuth2 = google.auth.OAuth2
+const handlebars = require('handlebars')
+const rootDir = require('../util/path')
+const path = require('path')
 
 const mailer = async function(emailAddress, form, files) {
-  let htmlToSend = null;
+  let htmlToSend = null
 
   const readHTMLTemplate = function(path, callback) {
-    fs.readFile(path, { encoding: "utf-8" }, function(err, html) {
+    fs.readFile(path, { encoding: 'utf-8' }, function(err, html) {
       if (err) {
-        throw err;
+        throw err
       } else {
-        callback(null, html);
+        callback(null, html)
       }
-    });
-  };
-  
-  readHTMLTemplate(__dirname + "/email_template.html", function(err, html) {
-    const template = handlebars.compile(html);
+    })
+  }
+
+  readHTMLTemplate(__dirname + '/email_template.html', function(err, html) {
+    const template = handlebars.compile(html)
     // const replacements = {
-    //   user_name: "John Doe",
+    //   idName: "John Doe",
     //   socialName: "John Does",
-    //   user_email: "johndoe@email.com",
+    //   email: "johndoe@email.com",
     //   address: "trav. lomas valentias",
     //   contact: "9122334455",
-    //   date: "22/06/1996",
+    //   birthdate: "22/06/1996",
     //   cpf: "02178957650",
     //   nationality: "Brasileiro",
     //   sex: "Masculino",
-    //   doc: "7881423",
     //   deficiency: "Nenhuma",
     // };
-    htmlToSend = template(form);
+    htmlToSend = template(form)
     // console.log(htmlToSend);
-  });
+  })
 
   const oauth2Client = new OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
+    'https://developers.google.com/oauthplayground'
+  )
 
   oauth2Client.setCredentials({
     refresh_token: process.env.REFRESH_TOKEN,
-  });
+  })
 
   const accessToken = await new Promise((resolve, reject) => {
     oauth2Client.getAccessToken((err, token) => {
       if (err) {
-        reject("Failed to create access token :(");
+        reject('Failed to create access token :(')
       }
-      resolve(token);
-    });
-  });
+      resolve(token)
+    })
+  })
 
-  console.log("creating transporter...");
+  console.log('creating transporter...')
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     // host: "smtp.gmail.com",
     // port: 587,
     // secure: false, // true for 465, false for other ports
     auth: {
-      type: "OAuth2",
+      type: 'OAuth2',
       user: process.env.EMAIL,
       accessToken,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN
-    }
-  });
+      refreshToken: process.env.REFRESH_TOKEN,
+    },
+  })
 
   // let attachmentsObject = {};
 
   const attachments = files.map(fileName => {
     return {
       filename: fileName,
-      path: path.join(rootDir, "uploads", fileName)
-    };
-  });
+      path: path.join(rootDir, 'uploads', fileName),
+    }
+  })
 
   // send mail with defined transport object
   const info = await transporter.sendMail(
     {
       from: '"SEDEPTI" <sedepti.devs@gmail.com>', // sender address
       to: emailAddress, // list of receivers
-      subject: "Cadastro BC - Novo cadastro", // Subject line
+      subject: 'Cadastro BC - Novo cadastro', // Subject line
       // html: '<b>Hello world?</b>', // html body
       attachments: attachments,
-      html: htmlToSend
+      html: htmlToSend,
     },
     (err, info) => {
       if (err) {
-        console.log(err);
+        console.log(err)
       }
 
       files.forEach(file => {
-        fs.unlink(path.join(rootDir, "uploads", file), err => {
+        fs.unlink(path.join(rootDir, 'uploads', file), err => {
           if (err) {
-            console.error(err);
+            console.error(err)
           } else {
-            console.log("cleared");
+            console.log('cleared')
           }
-        });
-      });
+        })
+      })
     }
-  );
-};
+  )
+}
 
-module.exports = mailer;
+module.exports = mailer
